c1a7e387d722bdfc9564823b884db30e413a5d43
Dom Oliaro <domoliaro@gmail.com>
2018-05-04 19:15:48 -0600

    banner fix

--- wp-content/themes/mindblank/layout/brand.php
+++ wp-content/themes/mindblank/layout/brand.php
@@ -1,10 +1,7 @@
 <section class="brand">
-    <div class="row">
-        <div class="nav"></div>
-        <div class="col logo">
-            <a href="<?php echo home_url(); ?>">
-                <img height="225px" src="<?php echo get_template_directory_uri(); ?>/img/cvpg-logo-banner.png" alt="The Weekly Howl" class="logo-img img-responsive">
-            </a>
-        </div>
+    <div class="col logo">
+        <a href="<?php echo home_url(); ?>">
+            <img height="225px" src="<?php echo get_template_directory_uri(); ?>/img/cvpg-logo-banner.png" alt="The Weekly Howl" class="logo-img img-responsive">
+        </a>
     </div>
 </section>
