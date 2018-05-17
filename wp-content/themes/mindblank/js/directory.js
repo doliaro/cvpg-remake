94ecfcbfbf3a525359f2d40a5896a9ba622d3ecc
Dom Oliaro <domoliaro@gmail.com>
2018-05-17 17:36:38 -0600

    fix

--- wp-content/themes/mindblank/js/directory.js
+++ wp-content/themes/mindblank/js/directory.js
@@ -17,14 +17,13 @@ function hide_show_facets(facet_id, facet_class){
     });
 }
 
-
 jQuery(document).ready(function() {
     setTimeout(function() {
         jQuery('.facetwp-autocomplete-update').attr('value', 'Go!');
-        jQuery('.facetwp-checkbox[data-value=region_1]').append("<p>Montclair, Pomona, Fontana, Chino Hills, San Bernardino County and surrounding areas</p>");
-        jQuery('.facetwp-checkbox[data-value=region_2]').append("<p>Covina, West Covina, Baldwin Park, El Monte, La Puente, Glendora, Azusa, Hacienda Heights, Rowland Heights and Whittier</p>");
-        jQuery('.facetwp-checkbox[data-value=region_3]').append("<p>Arcadia, Duarte and Monrovia</p>");
-        jQuery('.facetwp-checkbox[data-value=region_4]').append("<p>San Gabriel Valley, Inland Empire, Glendale and San Fernando Valley</p>");
-        jQuery('.facetwp-checkbox[data-value=region_5]').append("<p>Long Beach, Downey, Bellflower, Lakewood, Paramount and Hawaiian Gardens</p>");
+        jQuery('.facetwp-checkbox[data-value=1]').append("<p>Montclair, Pomona, Fontana, Chino Hills, San Bernardino County and surrounding areas</p>");
+        jQuery('.facetwp-checkbox[data-value=2]').append("<p>Covina, West Covina, Baldwin Park, El Monte, La Puente, Glendora, Azusa, Hacienda Heights, Rowland Heights and Whittier</p>");
+        jQuery('.facetwp-checkbox[data-value=3]').append("<p>Arcadia, Duarte and Monrovia</p>");
+        jQuery('.facetwp-checkbox[data-value=4]').append("<p>San Gabriel Valley, Inland Empire, Glendale and San Fernando Valley</p>");
+        jQuery('.facetwp-checkbox[data-value=5]').append("<p>Long Beach, Downey, Bellflower, Lakewood, Paramount and Hawaiian Gardens</p>");
     }, 100);
 });
