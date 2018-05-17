41868d7c613ff4866888504e04e7e299d3fabe8a
Dom Oliaro <domoliaro@gmail.com>
2018-05-17 15:06:38 -0600

    dropdown menu

--- wp-content/themes/mindblank/layout/brand.php
+++ wp-content/themes/mindblank/layout/brand.php
@@ -1,7 +1,7 @@
 <section class="brand">
     <div class="col logo">
-        <a href="<?php echo home_url(); ?>">
+        <!-- <a href="<?php echo home_url(); ?>"> -->
             <img height="225px" src="<?php echo get_template_directory_uri(); ?>/img/cvpg-logo-banner.png" alt="The Weekly Howl" class="logo-img img-responsive">
-        </a>
+        <!-- </a> -->
     </div>
 </section>
